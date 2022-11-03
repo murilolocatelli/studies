bb44c2ba76559de8a69f6d982a40a573d0eb38ce
murilolocatelli <murilolocatelli@gmail.com>
2022-11-03 14:28:15 -0300

    .

--- axios/main.js
+++
@@ -1,7 +0,0 @@
-axios.get("https://api.github.com/users/murilolocatelli")
-    .then(function(response) {
-        console.log(response.data.avatar_url);
-    })
-    .catch(function(error) {
-        console.warn(error)
-    });
